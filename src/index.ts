3a2153753045e464bdb060267fba6bb38bd6ee66
krisnabimantoro <krisnabmntr@gmail.com>
2024-07-31 18:04:07 +0700

    Fix bugs for release 1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
@@ -6,46 +6,45 @@ const port = 3000;
 app.use(express.json());
 
 // Endpoint untuk register user
-app.post('/register', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    // Logika untuk register user
-    res.send('User registered');
+app.post("/register", (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  // Logika untuk register user
+  res.send("User registered");
 });
 
 // Endpoint untuk login user
-app.post('/login', (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    // Logika untuk login user
-    res.send('User logged in');
+app.post("/login", (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  // Logika untuk login user
+  res.send("User logged in");
 });
 
 // Endpoint untuk menambah produk
-app.post('/products', (req: Request, res: Response) => {
-    const { name, price } = req.body;
-    // Logika untuk menambah produk
-    res.send('Product added');
+app.post("/products", (req: Request, res: Response) => {
+  const { name, price } = req.body;
+  // Logika untuk menambah produk
+  res.send("Product added");
 });
 
 // Endpoint untuk mendapatkan semua produk
-app.get('/products', (req: Request, res: Response) => {
-    // Logika untuk mendapatkan semua produk
-    res.send('List of products');
+app.get("/products", (req: Request, res: Response) => {
+  // Logika untuk mendapatkan semua produk
+  res.send("List of products");
 });
 
 // Endpoint untuk membuat order
-app.post('/orders', (req: Request, res: Response) => {
-    const { productId, quantity } = req.body;
-    // Logika untuk membuat order
-    res.send('Order created');
+app.post("/orders", (req: Request, res: Response) => {
+  const { productId, quantity } = req.body;
+  // Logika untuk membuat order
+  res.send("Order created");
 });
 
 // Endpoint untuk mendapatkan semua order
-app.get('/orders', (req: Request, res: Response) => {
-    // Logika untuk mendapatkan semua order
-    res.send('List of orders');
+app.get("/orders", (req: Request, res: Response) => {
+  // Logika untuk mendapatkan semua order
+  res.status(200).send("List of orders");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
-
